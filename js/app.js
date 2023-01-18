96ed7f00cd504e46d690eeed182a5773fb6311b2
Marawanelhaggar1 <Marawanmagdy91@gmail.com>
2023-01-18 06:57:09 +0200

    first

--- js/app.js
+++ js/app.js
@@ -1,60 +1,30 @@
 /**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
  * Define Global Variables
  *
  */
-const ul = document.getElementById("navbar__list");
-const sections = document.querySelectorAll("section");
+let ul = document.getElementById("navbar__list");
 const fragment = document.createDocumentFragment();
+const sections = document.querySelectorAll("section");
 
 /**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
+ * End Global Variables*/
 
 // build the nav
 for (var i = 1; i <= sections.length; i++) {
-  const li = document.createElement("li");
   const a = document.createElement("a");
+  const li = document.createElement("li");
   a.classList.add("menu__link");
   a.textContent = "Section" + " " + i;
-  //   a.setAttribute("id", `section${i}`);
+  // Scroll to anchor ID using scrollTO event
   a.setAttribute("href", `#section${i}`);
   li.appendChild(a);
   fragment.appendChild(li);
 }
 ul.appendChild(fragment);
+
 // Add class 'active' to section when near top of viewport
 
-// Scroll to anchor ID using scrollTO event
 const links = document.querySelectorAll("a.menu__link");
-
 const callBack = (entries) => {
   let section = entries[0];
   section.target.classList.remove("your-active-class");
@@ -78,20 +48,51 @@ const options = {
   threshold: 0.3,
 };
 
+// Set sections as active
 const observer = new IntersectionObserver(callBack, options);
 window.addEventListener("scroll", () => {
   sections.forEach((section) => {
     observer.observe(section);
   });
 });
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
 
 // Scroll to section on link click
+let prevScrollpos;
+window.addEventListener("scroll", () => {
+  const header = document.getElementById("header");
 
-// Set sections as active
+  const currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    header.style.top = "0";
+  } else {
+    header.style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+});
+
+// Build menu
+const icon = document.querySelector("a");
+icon.addEventListener("click", () => {
+  if (ul.style.display === "none") {
+    ul.style.display = "block";
+  } else {
+    ul.style.display = "none";
+  }
+});
+
+// const  = ul.querySelectorAll("a")
+
+// sections.forEach(function (section) {
+//   links.forEach((link) => {
+//     link.addEventListener("click", (evt) => {
+//       evt.preventDefault();
+//       section.scrollIntoView({
+//         behavior: "smooth",
+//         block: "end",
+//         inline: "nearest",
+//       });
+//     });
+//   });
+// });
+
+document.documentElement.style.scrollBehavior = "smooth";
